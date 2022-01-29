eee41268401b90a2c98381478c50da11a1b0e66a
mchaitanya <chaitanyaaa@gmail.com>
2022-01-29 12:24:20 -0800

    Add handler for the '/add-product' route.

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>The 'Add Product' Page</h1>");
+});
+
+// '/' matches all paths since all paths start with a '/'.
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express</h1>");
 });
 
