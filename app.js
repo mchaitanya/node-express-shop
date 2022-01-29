b780c8c330ccb9b3f4fad31d8a5bb4258fae5906
mchaitanya <chaitanyaaa@gmail.com>
2022-01-29 12:00:48 -0800

    Install express.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<html>");
-  res.write("<head><title>My App</title></head>");
-  res.write("<body><h1>Hello, world</h1></body>");
-  res.write("</html>");
-  res.end();
+const app = express();
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server running at http://localhost:3000/");
 });
