5d84709cf7e6cccf5edf635c918835d7d299d764
mchaitanya <chaitanyaaa@gmail.com>
2022-01-30 12:18:50 -0800

    Serve the HTML pages in the response.

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 // GET /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.send(`
-  <form action="/admin/add-product" method="POST">
-    <input name="title"/><button type="submit">Add product</button>
-  </form>`);
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 // POST /admin/add-product
--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
@@ -5,9 +7,8 @@ const router = express.Router();
 // The use method does a prefix match on the path.
 // But the get, post etc. methods do a full match.
 router.get("/", (req, res, next) => {
-  res.send(`
-  <nav><a href="/">Home</a> | <a href="/admin/add-product">Add product</a></nav>
-  <h1>Hello from Express</h1>`);
+  // __dirname is a global variable that points to the directory that contains the file it is used in i.e. 'routes'
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
