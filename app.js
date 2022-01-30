2e95f061531ec2caaf142d6399316144632378da
mchaitanya <chaitanyaaa@gmail.com>
2022-01-29 18:05:36 -0800

    Add form on '/add-product' page.

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const express = require("express");
 const app = express();
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>The 'Add Product' Page</h1>");
+  res.send(`
+<form action="/product" method="POST">
+  <input name="title"/><button type="submit">Add product</button>
+</form>`);
+});
+
+app.post("/product", (req, res) => {
+  res.redirect("/");
 });
 
 // '/' matches all paths since all paths start with a '/'.
 app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
+  res.send(`
+<nav><a href="/">Home</a> | <a href="/add-product">Add product</a></nav>
+<h1>Hello from Express</h1>`);
 });
 
 app.listen(3000, () => {
