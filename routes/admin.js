baa9cde0b7811a816076d3573bab1b00f9ea9569
mchaitanya <chaitanyaaa@gmail.com>
2022-01-30 12:38:17 -0800

    Refactor how the path to the views is constructed.
    `require.main.path` returns the directory path of 'app.js'
    and we can construct other paths relative to it.

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // GET /admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(require.main.path, "views", "add-product.html"));
 });
 
 // POST /admin/add-product
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,9 @@ const router = express.Router();
 // But the get, post etc. methods do a full match.
 router.get("/", (req, res, next) => {
   // __dirname is a global variable that points to the directory that contains the file it is used in i.e. 'routes'
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  // `require.main.path` returns the directory path of the entry module
+  //   res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(require.main.path, "views", "shop.html"));
 });
 
 module.exports = router;
