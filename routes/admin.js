50ed71cbdb6f19a3a8617da28aecb7a431143a8d
mchaitanya <chaitanyaaa@gmail.com>
2022-02-27 18:01:05 -0800

    Set the active class on the nav link for the loaded page.

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const products = [];
 // GET /admin/add-product
 router.get("/add-product", (req, res, next) => {
   // res.sendFile(path.join(require.main.path, "views", "add-product.html"));
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 // POST /admin/add-product
