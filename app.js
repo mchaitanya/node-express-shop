bc25e6770778fa4c8cd8776dd7c74e6e65bfb5dd
mchaitanya <chaitanyaaa@gmail.com>
2022-03-13 22:37:38 -0700

    Switch to ejs templates.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { routes: adminRoutes } = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 // Built-in middleware that allows access to static files in the 'public' directory
